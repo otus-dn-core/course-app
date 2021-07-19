22dab6727d4927edcaa957ca6406a8d8d73bb593
otus-dn-core <otus@dn-core.ru>
2021-07-19 22:16:23 +0300

    Form-field Validators WELL, before Password

--- src/app/pages/demo/pages/shared/shared.component.ts
+++ src/app/pages/demo/pages/shared/shared.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { regex, regexErrors } from '@app/shared';
+
 @Component({
   selector: 'app-shared',
   templateUrl: './shared.component.html',
   styleUrls: ['./shared.component.scss']
 })
 export class SharedComponent implements OnInit {
-  
-  constructor(private fb: FormBuilder) { }
 
-  form!: FormGroup;
+  form: FormGroup;
+  isInline: boolean;
+  regexErrors = regexErrors;
+  
+  constructor(private fb: FormBuilder) {
+    this.isInline = true;
+   }
 
   ngOnInit(): void {
-      this.form = this.fb.group({
-        input: [null]
-      });
+    this.form = this.fb.group({
+      input: [null,{
+        updateOn: 'blur',
+        validators: [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.pattern(regex.numbers)
+        ]
+      }]
+    });
   }
 
   onSubmit(): void {
@@ -25,5 +38,9 @@ export class SharedComponent implements OnInit {
 
   onPatchValue(): void {
       this.form.patchValue({input: 'test'});
+  }
+
+  onToggleInline() {
+    this.isInline = !this.isInline;
   }
 }
